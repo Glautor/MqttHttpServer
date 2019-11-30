d2bb6df1f7a909b64f02c4c977b6b22c89dc60c6
fernandesleticia <leticiafernandes777@gmail.com>
2019-11-29 23:18:06 -0300

    adiciona pub e sub RabbitMQ

---
+++ sub.js
@@ -0,0 +1,24 @@
+var amqp = require('amqplib/callback_api');
+
+
+amqp.connect('amqp://localhost:5672', function (err, conn) {
+
+    //Exchange que está assinando
+    conn.createChannel(function (err, ch) {
+        var ex = 'pub_sub_meetup28';
+
+        ch.assertExchange(ex, 'fanout', { durable: false });
+
+        ch.assertQueue('', { exclusive: true }, function (err, q) {
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+            ch.bindQueue(q.queue, ex, '');
+
+            //consumindo os dados da fila
+            ch.consume(q.queue, function (msg) {
+                console.log(" [x] %s", msg.content.toString());
+            }, { noAck: true });
+        });
+    });
+
+
+})
